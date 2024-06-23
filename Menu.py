f14ddce3728fba809b02fdfe114fcd66b2bfd44b
SHREK™ <161427467+SHREK-TM@users.noreply.github.com>
2024-06-24 00:35:05 +0100

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -1,7 +1,7 @@
-# Shrek was proudly coded by Shrek™ [https://github.com/blackray207].
+# Shrek was proudly coded by Shrek™ [https://github.com/SHREK-TM].
 # Copyright © Shrek Multi Tools
 
-########################################
+####################################################################
 
 from utilities.Settings.common import *
 from utilities.Settings.common2 import *
