f8f0b6c97a740933532021434df4516accff60b6
SHREK™ <161427467+SHREK-TM@users.noreply.github.com>
2024-08-05 13:29:31 +0100

    [MODULE ADDED]fix-error

---
+++ utilities/Settings/module.py
@@ -0,0 +1,45 @@
+import subprocess
+
+packages = [
+    "requests",
+    "setuptools",
+    "colorama",
+    "bs4",
+    "selenium",
+    "discord",
+    "discum",
+    "pyinstaller",
+    "qrcode",
+    "python-http-client",
+    "charset_normalizer",
+    "2captcha-python",
+    "beautifulsoup4",
+    "pyperclip",
+    "pypiwin32",
+    "packaging",
+    "keyboard",
+    "colored",
+    "PyNaCl",
+    "easygui",
+    "tasksio",
+    "colour",
+    "pillow",
+    "psutil",
+    "emoji",
+    "httpx",
+    "tqdm",
+    "websocket",
+    "Cipher",
+    "Popen",
+    "login",
+    "pipe",
+    "fore",
+    "aes",
+    "loads",
+    "pystyle",
+    "pyautogui",
+    "pyfadecolor"
+]
+
+for package in packages:
+    subprocess.run(["pip", "install", package])
