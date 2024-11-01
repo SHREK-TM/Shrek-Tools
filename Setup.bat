c2cbcc724736876b6b6f2e17c675552c464fbcd2
SHREK™ <161427467+SHREK-TM@users.noreply.github.com>
2024-11-01 19:17:02 +0100

    [UPDATE]

--- Setup.bat
+++ Setup.bat
@@ -1,17 +1,39 @@
 @echo off
-Title Download Modules...
-python --version 3>NUL
-if errorlevel 1 goto errorNoPython
-pip -v>NUL
-if errorlevel 1 goto errorNoPip
-pip install subprocess
+Title Download Library...
+
+python --version >nul 2>&1
+if errorlevel 1 (
+    echo Warning: Python is not installed or not in the PATH.
+    pause
+    goto :eof
+)
+
+pip -V >nul 2>&1
+if errorlevel 1 (
+    echo Warning: Pip is not installed or not in the PATH.
+    pause
+    goto :eof
+)
+
 cd utilities\Settings
 python module.py
-cd ..
-cd ..
-move utilities\Start.bat .
-cd utilities\Settings
-cd ..
-cd ..
+cd ..\..
+
+move utilities\Start.bat . >nul 2>&1
+
 cls
-Start.bat
+set LIBRARIES=pylibcheck requests setuptools colorama bs4 selenium discord discum pyinstaller qrcode python-http-client charset_normalizer 2captcha-python beautifulsoup4 pyperclip pypiwin32 packaging keyboard colored PyNaCl easygui tasksio colour pillow psutil emoji httpx tqdm websocket Cipher Popen login pipe fore aes loads pystyle pyautogui pyfadecolor
+
+for %%L in (%LIBRARIES%) do (
+    python -c "import %%L" >nul 2>&1
+    if %errorlevel% neq 0 (
+        pip install %%L
+    )
+)
+
+if exist Start.bat (
+    Start Start.bat
+) else (
+    echo Warning: Start.bat not found.
+    pause
+)
