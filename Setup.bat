a1ca438c79883e244fc31dd9536c98ea74dc16b3
SHREK™ <161427467+SHREK-TM@users.noreply.github.com>
2025-01-12 23:40:09 +0100

    [UPDATE]

--- Setup.bat
+++ Setup.bat
@@ -1,13 +1,11 @@
 @echo off
 Title Download Library...
 
-:: Vérification si Python est dans le PATH
 where python >nul 2>&1
 if errorlevel 1 (
     echo Error: Python is not added to PATH.
     set /p choice=Do you want to add Python to PATH? (Y/N): 
     if /i "%choice%"=="Y" (
-        :: Recherche du chemin d'installation de Python
         for /f "tokens=2*" %%a in ('reg query "HKCU\Software\Python\PythonCore" /s /v "InstallPath" 2^>nul ^| find "InstallPath"') do (
             set PYTHON_PATH=%%b
         )
@@ -16,7 +14,6 @@ if errorlevel 1 (
                 set PYTHON_PATH=%%b
             )
         )
-
         if defined PYTHON_PATH (
             echo Adding Python to PATH...
             setx PATH "%PATH%;%PYTHON_PATH%"
@@ -33,7 +30,6 @@ if errorlevel 1 (
     )
 )
 
-:: Vérification de la version de Python
 for /f "tokens=2 delims= " %%v in ('python --version 2^>nul') do (
     for /f "tokens=1,2 delims=." %%i in ("%%v") do (
         if %%i lss 3 (
@@ -49,7 +45,6 @@ for /f "tokens=2 delims= " %%v in ('python --version 2^>nul') do (
     )
 )
 
-:: Vérification si pip est installé
 pip -V >nul 2>&1
 if errorlevel 1 (
     echo Error: Pip is not installed or not in the PATH.
@@ -60,21 +55,17 @@ if errorlevel 1 (
 cls
 echo Installing required libraries...
 
-:: Liste des bibliothèques Python à installer
 set LIBRARIES=setuptools==65.5.0 pip==22.3.1 wheel requests==2.26.0 colorama selenium==4.0.0 discord==2.5.1 pyinstaller qrcode python-http-client charset_normalizer==2.0.10 2captcha-python beautifulsoup4 pyperclip pypiwin32 packaging keyboard colored PyNaCl easygui tasksio colour pillow psutil emoji httpx tqdm websocket Cipher Popen login pipe fore aes loads pystyle pyautogui pyfadecolor
 
-:: Installation des bibliothèques
 for %%L in (%LIBRARIES%) do (
     python -m pip install "%%L" --upgrade --quiet
 )
 
-:: Création du fichier Start.bat
 (
     echo @echo off
     echo Python Menu.py
 ) > Start.bat
 
-:: Lancement de Start.bat
 if exist Start.bat (
     Start Start.bat
 ) else (
