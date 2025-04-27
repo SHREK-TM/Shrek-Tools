07e6f15d2a4f5d80999a9db1d773ee1e3a4eb6b0
SHREK™ <161427467+SHREK-TM@users.noreply.github.com>
2025-04-27 13:21:55 +0100

    [UPDATE]

--- Setup.bat
+++ Setup.bat
@@ -55,7 +55,7 @@ if errorlevel 1 (
 cls
 echo Installing required libraries...
 
-set LIBRARIES=setuptools==65.5.0 pip==22.3.1 wheel requests==2.26.0 colorama selenium==4.0.0 discord==2.5.1 pyinstaller qrcode python-http-client charset_normalizer==2.0.10 2captcha-python beautifulsoup4 pyperclip pypiwin32 packaging keyboard colored PyNaCl easygui tasksio colour pillow psutil emoji httpx tqdm websocket Cipher Popen login pipe fore aes loads pystyle pyautogui pyfadecolor
+set LIBRARIES=setuptools==65.5.0 pip==22.3.1 wheel requests==2.26.0 colorama selenium==4.0.0 discord.py==2.3.2 pyinstaller qrcode python-http-client charset_normalizer==2.0.10 2captcha-python beautifulsoup4 pyperclip pypiwin32 packaging keyboard colored PyNaCl easygui tasksio colour pillow psutil emoji httpx tqdm websocket Cipher Popen login pipe fore aes loads pystyle pyautogui 
 
 for %%L in (%LIBRARIES%) do (
     python -m pip install "%%L" --upgrade --quiet
