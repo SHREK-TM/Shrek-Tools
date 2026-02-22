b0919b189474d7907866cff018385da31b577b70
SHREK™ <161427467+SHREK-TM@users.noreply.github.com>
2026-02-22 16:18:54 +0000

    [UPDATE]

--- utilities/Plugins/Server_Lookup.py
+++ utilities/Plugins/Server_Lookup.py
@@ -1,97 +1,67 @@
-# Shrek was proudly coded by Shrek™ [https://github.com/SHREK-TM].
-# Copyright © Shrek Multi Tools
-
-####################################################################
-
 import os
-import sys
-import os.path
-import colorama
 import requests
-import webbrowser
+import colorama
 from time import sleep
-from colored import fg, attr
-from requests.api import options
-from colorama import Fore, Back, Style, init
+from colorama import Fore, init
 
-colorama.init(autoreset=True)
+init(autoreset=True)
 
 def cls():
-  os.system('cls' if os.name=='nt' else 'clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-def menu():
+def main():
     cls()
+    print(f"""{Fore.GREEN}
+          
+   ██▓    ▒█████   ▒█████   ▀██ ▄█▀ █    ██  ██▓███  
+  ▓██▒   ▒██▒  ██▒▒██▒  ██▒  ██▄█▒  ██  ▓██▒▓██░  ██ 
+  ▒██░   ▒██░  ██▒▒██░  ██▒ ▓███▄░ ▓██  ▒██░▓██░ ██▓▒
+  ▒██░   ▒██   ██░▒██   ██░ ▓██ █▄ ▓▓█  ░██░▒██▄█▓▒ ▒
+  ░██████░ ████▓▒░░ ████▓▒░ ▒██▒ █▄▒▒█████▓ ▒██▒ ░  ░
+  ░ ▒░▓  ░ ▒░▒░▒░ ░ ▒░▒░▒░  ▒ ▒▒ ▓▒ ▒▓▒ ▒ ▒ ▒▓▒░ ░  ░
+  ░ ░ ▒    ░ ▒ ▒░   ░ ▒ ▒░  ░ ░▒ ▒░ ░▒░ ░ ░ ░▒ ░     
+    ░ ░  ░ ░ ░ ▒  ░ ░ ░ ▒   ░ ░░ ░   ░░ ░ ░ ░░       
+      ░      ░ ░      ░ ░   ░  ░      ░                      
+    """)
+    
+    token = input(f"{Fore.GREEN}[{Fore.WHITE}+{Fore.GREEN}] {Fore.WHITE}Token: ")
+    guild_id = input(f"{Fore.GREEN}[{Fore.WHITE}+{Fore.GREEN}] {Fore.WHITE}Server ID: ")
+
+    headers = {
+        'Authorization': token,
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+    }
+
+    print(f"\n{Fore.YELLOW}Fetching data...{Fore.RESET}")
+    sleep(1)
+
+    res = requests.get(f"https://discord.com/api/v9/guilds/{guild_id}?with_counts=true", headers=headers)
+    
+    if res.status_code != 200:
+        print(f"{Fore.RED}Error: Could not find server. Check your Token and ID.")
+        return
+
+    data = res.json()
+    owner_id = data.get('owner_id')
+    owner_res = requests.get(f"https://discord.com/api/v9/guilds/{guild_id}/members/{owner_id}", headers=headers)
+    
+    owner_name = "Unknown"
+    if owner_res.status_code == 200:
+        owner_data = owner_res.json()
+        user = owner_data.get('user', {})
+        owner_name = f"{user.get('username')}#{user.get('discriminator', '0000')}"
+
     print(f"""
-{Fore.GREEN}
-  
-   ██▓    ▒█████   ▒█████   ▀██ ▄█▀ █    ██  ██▓███  
-  ▓██▒   ▒██▒  ██▒▒██▒  ██▒  ██▄█▒  ██  ▓██▒▓██░  ██ 
-  ▒██░   ▒██░  ██▒▒██░  ██▒ ▓███▄░ ▓██  ▒██░▓██░ ██▓▒
-  ▒██░   ▒██   ██░▒██   ██░ ▓██ █▄ ▓▓█  ░██░▒██▄█▓▒ ▒
-  ░██████░ ████▓▒░░ ████▓▒░ ▒██▒ █▄▒▒█████▓ ▒██▒ ░  ░
-  ░ ▒░▓  ░ ▒░▒░▒░ ░ ▒░▒░▒░  ▒ ▒▒ ▓▒ ▒▓▒ ▒ ▒ ▒▓▒░ ░  ░
-  ░ ░ ▒    ░ ▒ ▒░   ░ ▒ ▒░  ░ ░▒ ▒░ ░▒░ ░ ░ ░▒ ░     
-    ░ ░  ░ ░ ░ ▒  ░ ░ ░ ▒   ░ ░░ ░   ░░ ░ ░ ░░       
-      ░      ░ ░      ░ ░   ░  ░      ░              
+{Fore.GREEN}--------< Server Information >--------{Fore.RESET}
 
-{Fore.GREEN}[{Fore.WHITE}1{Fore.GREEN}] {Fore.WHITE}Server Lookup
-{Fore.GREEN}[{Fore.WHITE}0{Fore.GREEN}] {Fore.WHITE}Exit
+[{Fore.GREEN}Name{Fore.RESET}]      :   {data.get('name')} 
+[{Fore.GREEN}ID{Fore.RESET}]        :   {data.get('id')}
+[{Fore.GREEN}Owner{Fore.RESET}]     :   {owner_name} 
+[{Fore.GREEN}Owner ID{Fore.RESET}]  :   {owner_id}
+[{Fore.GREEN}Members{Fore.RESET}]   :   {data.get('approximate_member_count')}
+[{Fore.GREEN}Region{Fore.RESET}]    :   {data.get('region')}
+[{Fore.GREEN}Icon URL{Fore.RESET}]  :   https://cdn.discordapp.com/icons/{guild_id}/{data.get('icon')}.webp?size=256
 """)
-menu()
-
-option = input(f"""{Fore.GREEN}[{Fore.WHITE}>{Fore.GREEN}] {Fore.WHITE} """)
-
-def fetch_data(option):
-    if option == '1':
-        cls()
-        print(f"""
-{Fore.GREEN}
-   ██▓    ▒█████   ▒█████   ▀██ ▄█▀ █    ██  ██▓███  
-  ▓██▒   ▒██▒  ██▒▒██▒  ██▒  ██▄█▒  ██  ▓██▒▓██░  ██ 
-  ▒██░   ▒██░  ██▒▒██░  ██▒ ▓███▄░ ▓██  ▒██░▓██░ ██▓▒
-  ▒██░   ▒██   ██░▒██   ██░ ▓██ █▄ ▓▓█  ░██░▒██▄█▓▒ ▒
-  ░██████░ ████▓▒░░ ████▓▒░ ▒██▒ █▄▒▒█████▓ ▒██▒ ░  ░
-  ░ ▒░▓  ░ ▒░▒░▒░ ░ ▒░▒░▒░  ▒ ▒▒ ▓▒ ▒▓▒ ▒ ▒ ▒▓▒░ ░  ░
-  ░ ░ ▒    ░ ▒ ▒░   ░ ▒ ▒░  ░ ░▒ ▒░ ░▒░ ░ ░ ░▒ ░     
-    ░ ░  ░ ░ ░ ▒  ░ ░ ░ ▒   ░ ░░ ░   ░░ ░ ░ ░░       
-      ░      ░ ░      ░ ░   ░  ░      ░              
-              """) 
-        sleep(1)
-
-        headers = {
-            'User-Agent' : 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7',
-            'Authorization' : input(f"""{Fore.GREEN}[{Fore.WHITE}+{Fore.GREEN}] {Fore.WHITE}Token: """)
-        }
-
-        guildId = input(f"""{Fore.GREEN}[{Fore.WHITE}+{Fore.GREEN}] {Fore.WHITE}Server ID: """)
-
-        response = requests.get(
-            f"https://discord.com/api/guilds/{guildId}",
-            headers = headers,
-            params = {"with_counts" : True}
-        ).json()
-
-        owner = requests.get(
-            f"https://discord.com/api/guilds/{guildId}/members/{response['owner_id']}",
-            headers = headers,
-            params = {"with_counts" : True}
-        ).json()
-
-        print(f"""
-{Fore.RESET}{Fore.GREEN}--------< Server Infomation >--------{Fore.RESET}
-
-[{Fore.GREEN}Name{Fore.RESET}]      :   {response['name']} 
-[{Fore.GREEN}ID{Fore.RESET}]        :   {response['id']}
-[{Fore.GREEN}Owner{Fore.RESET}]     :   {owner['user']['username']}#{owner['user']['discriminator']} 
-[{Fore.GREEN}Owner ID{Fore.RESET}]  :   {response['owner_id']}
-[{Fore.GREEN}Members{Fore.RESET}]   :   {response['approximate_member_count']}
-[{Fore.GREEN}Region{Fore.RESET}]    :   {response['region']}
-[{Fore.GREEN}Icon URL{Fore.RESET}]  :   https://cdn.discordapp.com/icons/{guildId}/{response['icon']}.webp?size=256
-""")
-        return
-
-    elif option == '0':
-        return
 
 if __name__ == '__main__':
-    fetch_data(option)
+    main()
